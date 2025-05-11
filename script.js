10e67c7b8016e7a70b99816b82b0371bfffc1fb8
Lorena-Rinaldo <>
2025-05-11 19:04:47 -0300

    Iniciando as seções da main

--- script.js
+++ script.js
@@ -16,6 +16,23 @@ function ocultarMenu() {
 
 window.onload = function() {
     let index = 0;
+    const images = document.querySelectorAll('.carrosselConteudo img');
+    const totalImages = images.length;
+
+    function showNextImage() {
+        images.forEach((img, i) => {
+            img.style.opacity = (i === index) ? '1' : '0';
+        });
+        index = (index + 1) % totalImages;
+    }
+
+    showNextImage();
+    setInterval(showNextImage, 6000);
+};
+
+
+window.onload = function() {
+    let index = 0;
     const images = document.querySelectorAll('.publicidade img');
     const totalImages = images.length;
 
